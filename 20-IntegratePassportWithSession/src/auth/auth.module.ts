f2bb77acb1c2377f49f41f70e75cb71d8bcc01c2
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-06-15 07:16:28 +0700

    Integrate passport with session

---
+++ 20-IntegratePassportWithSession/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersService } from 'src/users/services/users/users.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './services/auth/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../typeorm';
+import { LocalStrategy } from './utils/LocalStrategy';
+import { SessionSerializer } from './utils/SessionSerializer';
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [AuthController],
+  providers: [
+    {
+      provide: 'AUTH_SERVICE',
+      useClass: AuthService,
+    },
+    {
+      provide: 'USER_SERVICE',
+      useClass: UsersService,
+    },
+    LocalStrategy,
+    SessionSerializer,
+  ],
+})
+export class AuthModule {}
---
+++ 20-IntegratePassportWithSession/src/users/services/users/users.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { plainToClass } from 'class-transformer';
+import { User as UserEntity } from 'src/typeorm';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { SerializedUser, User } from 'src/users/types';
+import { encodePassword } from 'src/utils/bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  private users: User[] = [
+    // {
+    //   id: 1,
+    //   username: 'Tien',
+    //   password: '1234',
+    // },
+    // {
+    //   id: 2,
+    //   username: 'Tien2',
+    //   password: '12342',
+    // },
+    // {
+    //   id: 3,
+    //   username: 'Tien3',
+    //   password: '12343',
+    // },
+    // {
+    //   id: 4,
+    //   username: 'Tien4',
+    //   password: '12344',
+    // },
+  ];
+
+  getUsers() {
+    return this.users.map((user) => plainToClass(SerializedUser, user));
+  }
+
+  getUserByUsername(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const password = encodePassword(createUserDto.password);
+    console.log(password);
+    const newUser = this.userRepository.create({ ...createUserDto, password });
+    return this.userRepository.save(newUser);
+  }
+
+  findUserByUsername(username: string) {
+    return this.userRepository.findOneBy({ username });
+  }
+  findUserById(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+}
